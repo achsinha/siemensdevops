5c4e89059a69a2bf55782a8295965823f7a2efa4
nileshdevdas <nilesh.devdas@gmail.com>
2019-06-20 11:07:39 +0530

    for webhook

--- src/main/java/com/example/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/example/devopsdemo/DevopsdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsdemoApplication {
 
 	public static void main(String[] args) {
+		//For code commit
 		SpringApplication.run(DevopsdemoApplication.class, args);
 	}
 
